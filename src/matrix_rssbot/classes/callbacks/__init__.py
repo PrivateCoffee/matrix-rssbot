ebb9b245a8cdafde43d4f03ef85d442f8725a49d
Kumi <git@kumi.email>
2024-03-02 15:50:59 +0100

    Remove unused Join callback

--- src/matrix_rssbot/classes/callbacks/__init__.py
+++ src/matrix_rssbot/classes/callbacks/__init__.py
@@ -12,13 +12,11 @@ from nio import (
 
 from .sync import sync_callback
 from .invite import room_invite_callback
-from .join import join_callback
 from .message import message_callback
 from .roommember import roommember_callback
 
 RESPONSE_CALLBACKS = {
     SyncResponse: sync_callback,
-    JoinResponse: join_callback,
 }
 
 EVENT_CALLBACKS = {
